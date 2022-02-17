d90957dfce21a1f648c5de70c076ea17d9226a27
Tejaswini N <nteju37@gmail.com>
2022-02-17 11:00:00 +0530

    started iOS

--- AppOGee/AppOGee/controller/LoginViewController.swift
+++ AppOGee/AppOGee/controller/LoginViewController.swift
@@ -10,13 +10,47 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var bg_img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = true
+       // animate(self.imageview)
+        animateBackground()
+    }
+    func animateBackground() {
+        let animationOptions = UIView.AnimationOptions.repeat
+        let backgroundImage = UIImage(named:"Oen1Hm0.jpeg")!
+        var amountToKeepImageSquare = backgroundImage.size.height - self.view.frame.size.height
+
+        // UIImageView 1
+        var backgroundImageView1 = UIImageView(image: backgroundImage)
+        backgroundImageView1.frame = CGRect(x: self.view.frame.origin.x, y: self.view.frame.origin.y, width: backgroundImage.size.width - amountToKeepImageSquare, height: self.view.frame.size.height)
+        
+        self.view.addSubview(backgroundImageView1)
+
+        // UIImageView 2
+        var backgroundImageView2 = UIImageView(image: backgroundImage)
+        backgroundImageView2.frame = CGRect(x: backgroundImageView1.frame.size.width, y: self.view.frame.origin.y, width: backgroundImage.size.width - amountToKeepImageSquare, height: self.view.frame.height)
+        self.view.addSubview(backgroundImageView2)
+
+        // Animate background
+        UIView.animate(withDuration: 6.0, delay: 0.0, options: animationOptions, animations: {
+            backgroundImageView1.frame = backgroundImageView1.frame.offsetBy(dx: -1 * backgroundImageView1.frame.size.width, dy: 0.0)
+            backgroundImageView2.frame = backgroundImageView2.frame.offsetBy(dx: -1 * backgroundImageView2.frame.size.width, dy: 0.0)
+            }, completion: nil)
+
+       
+    }
+    
+    func animate(_ image: UIImageView) {
+        UIView.animate(withDuration: 12.0, delay: 0.0, options: [.repeat, .curveLinear], animations: {
+                self.bg_img.frame = self.bg_img.frame.offsetBy(dx: -1 * self.bg_img.frame.size.width, dy: 0.0)
+                self.imageview.frame = self.imageview.frame.offsetBy(dx: -1 * self.imageview.frame.size.width, dy: 0.0)
+            }, completion: nil)
     }
 }
